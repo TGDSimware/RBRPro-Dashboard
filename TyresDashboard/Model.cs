54d88c6ddfa531440d2cc5b9280dd3b2f16225a1
Grégory Tailly <gregory.tailly@outlook.fr>
2021-01-17 02:12:24 +0100

    init tyres dash

---
+++ TyresDashboard/Model.cs
@@ -0,0 +1,119 @@
+﻿using RBRPro.Api;
+using System.ComponentModel;
+using TGD.Framework;
+using System.Windows.Media.Imaging;
+
+namespace RBRProDashboard
+{
+    public class Model : DynamicContextManager
+    {
+        //static IniFile _otherIni = new IniFile("OtherIniFile.ini");
+
+        /// <summary>
+        /// The Persistent Properties are loaded and saved automatically. They can have a default value. 
+        /// Only C# base types only (the framework cannot persist complex types)
+        /// </summary>
+        #region PERSISTENT PROPERTIES
+        [DefaultValue(true)]
+        [ConfigProperty]
+        public bool ShowCarSpeed { get { return GetIniProperty<bool>(); } set { SetIniProperty(value); } }
+
+        /// <summary>
+        /// This property is mapped in a specific section of the .ini File
+        /// </summary>
+        [ConfigProperty("Section2")] 
+        public string AnotherPersistentProperty { get { return GetIniProperty<string>(); } set { SetIniProperty(value); } }
+
+        /// <summary>
+        /// This property is mapped in a specific section of the .ini File with a specific name
+        /// </summary>
+        [ConfigProperty("Section2", "PropertyName")]
+        public string YetAnotherPersistentProperty { get { return GetIniProperty<string>(); } set { SetIniProperty(value); } }
+
+        [DefaultValue(120)]
+        [ConfigProperty("Overlay", "Top")]
+        public double OverlayTop { get { return GetIniPropertyNoCache<double>(); } set { SetIniProperty(value); } }
+
+        [DefaultValue(120)]
+        [ConfigProperty("Overlay", "Left")]
+        public double OverlayLeft { get { return GetIniPropertyNoCache<double>(); } set { SetIniProperty(value); } }
+
+        /// <summary>
+        /// This property is mapped in a specific section of the .ini File with a specific name
+        /// </summary>
+        /*[ConfigProperty]
+        public string AdditionalPersistentProperty { get { return GetIniProperty<string>(_otherIni); } set { SetIniProperty(_otherIni, value); } }*/
+        #endregion
+
+        /// <summary>
+        /// The Runtime Properties are the volatile ones. No limitations about the types: complex types and collections are allowed
+        /// </summary>
+        #region RUNTIME PROPERTIES
+
+        //TEMPERATURE PNEU GAUCHE AV float
+        [RuntimeProperty("Tyre.LeftFrontTemperature")]
+        public float LeftFrontTemperature { get { return GetRuntimeProperty<float>(); } set { SetRuntimeProperty(value); } }
+
+        //PRESSION PNEU GAUCHE AV float
+        [RuntimeProperty("Tyre.LeftFrontPressure")]
+        public float LeftFrontPressure { get { return GetRuntimeProperty<float>(); } set { SetRuntimeProperty(value); } }
+
+        //USURE PNEU GAUCHE AV float
+        [RuntimeProperty("Tyre.LeftFrontDamage")]
+        public float LeftFrontDamage { get { return GetRuntimeProperty<float>(); } set { SetRuntimeProperty(value); } }
+
+
+        //TEMPERATURE PNEU DROIT AV float
+        [RuntimeProperty("Tyre.RightFrontTemperature")]
+        public float RightFrontTemperature { get { return GetRuntimeProperty<float>(); } set { SetRuntimeProperty(value); } }
+
+        //PRESSION PNEU DROIT AV float
+        [RuntimeProperty("Tyre.RightFrontPressure")]
+        public float RightFrontPressure { get { return GetRuntimeProperty<float>(); } set { SetRuntimeProperty(value); } }
+
+        //USURE PNEU DROIT AV float
+        [RuntimeProperty("Tyre.RightFrontDamage")]
+        public float RightFrontDamage { get { return GetRuntimeProperty<float>(); } set { SetRuntimeProperty(value); } }
+
+
+        //PRESSION PNEU GAUCHE AR
+        [RuntimeProperty("Tyre.LeftRearPressure")]
+        public float LeftRearPressure { get { return GetRuntimeProperty<float>(); } set { SetRuntimeProperty(value); } }
+
+        //TEMPERATURE PNEU GAUCHE AR
+        [RuntimeProperty("Tyre.LeftRearTemperature")]
+        public float LeftRearTemperature { get { return GetRuntimeProperty<float>(); } set { SetRuntimeProperty(value); } }
+
+        //USURE PNEU GAUCHE AR
+        [RuntimeProperty("Tyre.LeftRearDamage")]
+        public float LeftRearDamage { get { return GetRuntimeProperty<float>(); } set { SetRuntimeProperty(value); } }
+
+
+        //TEMPERATURE PNEU DROIT AR
+        [RuntimeProperty("Tyre.RightRearTemperature")]
+        public float RightRearTemperature { get { return GetRuntimeProperty<float>(); } set { SetRuntimeProperty(value); } }
+
+        //PRESSION PNEU DROIT AR
+        [RuntimeProperty("Tyre.RightRearPressure")]
+        public float RightRearPressure { get { return GetRuntimeProperty<float>(); } set { SetRuntimeProperty(value); } }
+
+        //USURE PNEU DROIT AR
+        [RuntimeProperty("Tyre.RightRearDamage")]
+        public float RightRearDamage { get { return GetRuntimeProperty<float>(); } set { SetRuntimeProperty(value); } }
+
+
+        /*[RuntimeProperty]
+        public int AnotherRuntimeProperty { get { return GetRuntimeProperty<int>(); } set { SetRuntimeProperty(value); } }*/
+        #endregion
+
+        /// <summary>
+        /// The file TestAddOn.ini is used as the main repository of the persistent properties
+        /// </summary>
+        public Model(TyresDashboardAddon addon) : base($"{AddOns.BASEPATH}\\{addon.Name}\\{addon.Name}.ini")
+        {
+            // Other model initialization code here
+        }
+
+
+    }
+}
---
+++ TyresDashboard/TyresDashboardAddon.cs
@@ -0,0 +1,107 @@
+﻿using RBRPro.Api;
+using System;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Media.Imaging;
+using TGD.Rbr.Telemetry.Data;
+
+namespace RBRProDashboard
+{
+    /// <summary>
+    /// The TestAddon
+    /// A RBRPro Addon is a class implementing the IRbrProAddOn interface.
+    /// Optionally, the Addon can receive telemetry by implementing the ITelemetryClient interface
+    /// </summary>
+    public class TyresDashboardAddon : IRbrProAddOn
+    {
+        // Maybe in future these properties will be replaced by class attributes
+        #region ABOUT
+        public string Name { get => "TyresDashboard"; }
+        public string Description { get => "This is a Test Add-On"; }
+        public string Author { get => "TGD"; }
+        public readonly char[] Gears = { 'R', 'N', '1', '2', '3', '4', '5', '6' };
+
+        // An optional icon is provided to the manager, just to decorate the tab item a bit...
+        public Image Icon => new Image { Source = new BitmapImage(new Uri($"pack://application:,,,/TyresDashboard;component/icon.png", UriKind.Absolute)) };
+
+        // This property tells the manager if the addon can be detached in a separate window or not
+        public bool IsDetachable { get => true; }
+
+        #endregion
+
+        // The interface used to interact with the manager
+        public IRbrPro _interactor;
+
+        // The viewmodel class
+        Model _model;
+
+        public TyresDashboardAddon()
+        {
+            _model = new Model(this);       
+        }
+
+        /// <summary>
+        /// Called by the manager just after the addon loading, the contents are not ready yet...
+        /// but in the meantime you can do something to prepare...
+        /// for example we can load our language strings
+        /// </summary>
+        public void Init(IRbrPro rbrProInteractor)
+        {
+            _interactor = rbrProInteractor;
+            _interactor.DataReceived += _interactor_DataReceived;
+        }
+
+        private void _interactor_DataReceived(object sender, TelemetryData data)
+        {
+            // this is where the telemetry is received, the data object contais all the infos
+
+            //PRESSURE 216340.858
+            //TEMPERATURE 337 DEGRés
+            //USURE 0.0036796
+
+
+            _model.LeftFrontDamage = data.car.suspensionLF.wheel.tire.segment1.wear;
+            _model.RightFrontDamage = data.car.suspensionLF.wheel.tire.segment1.wear;
+            _model.LeftRearDamage = data.car.suspensionLF.wheel.tire.segment1.wear;
+            _model.RightRearDamage = data.car.suspensionLF.wheel.tire.segment1.wear;
+
+            _model.LeftFrontPressure = data.car.suspensionLF.wheel.tire.pressure;
+            _model.RightFrontPressure = data.car.suspensionRF.wheel.tire.pressure;
+            _model.LeftRearPressure = data.car.suspensionLB.wheel.tire.pressure;
+            _model.RightRearPressure = data.car.suspensionRB.wheel.tire.pressure;
+
+            _model.LeftFrontTemperature = data.car.suspensionLF.wheel.tire.temperature;
+            _model.RightFrontTemperature = data.car.suspensionRF.wheel.tire.temperature;
+            _model.LeftRearTemperature = data.car.suspensionLB.wheel.tire.temperature;
+            _model.RightRearTemperature = data.car.suspensionRB.wheel.tire.temperature;
+        }
+
+        /// <summary>
+        /// Called by the manager when it finishes the initialization and the contents are available
+        /// </summary>
+        /// <param name="rbrProInteractor"></param>
+        public void Ready(IRbrPro rbrProInteractor)
+        {                
+
+        }
+
+        /// <summary>
+        /// Called by the manager, this method just returns an instance of the GUI Control
+        /// </summary>
+        /// <returns></returns>
+        public System.Windows.Controls.Control GetGui()
+        {
+            return new TyresDashboardGui(this, _interactor, _model);
+        }
+
+        public void Exit()
+        {
+            // No action performed
+        }
+
+        public void OnParentWindowChange(Window parent)
+        {
+            // No action performed
+        }
+    }
+}
